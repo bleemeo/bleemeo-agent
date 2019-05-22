b76650cb16735d050661bf94dbd492d33e054345
Pierre Fersing <pierre.fersing@bleemeo.com>
2019-05-22 16:22:16 +0200

    Use wheel and update package on Windows

--- packaging/windows/pywin32_paths.py
+++ packaging/windows/pywin32_paths.py
@@ -8,8 +8,8 @@ sys.path.extend([
 os.environ['PATH'] += (';' + os.path.join(scriptdir, 'Python'))
 
 # Preload pywintypes and pythoncom
-pwt = os.path.join(pkgdir, 'pypiwin32_system32', 'pywintypes36.dll')
-pcom = os.path.join(pkgdir, 'pypiwin32_system32', 'pythoncom36.dll')
+pwt = os.path.join(pkgdir, 'pywin32_system32', 'pywintypes37.dll')
+pcom = os.path.join(pkgdir, 'pywin32_system32', 'pythoncom37.dll')
 import imp
 imp.load_dynamic('pywintypes', pwt)
 imp.load_dynamic('pythoncom', pcom)
